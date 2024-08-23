c412ed4680c1d4c8138743daa63688725bf49739
dmun <davidru01@live.nl>
2024-08-23 22:43:25 +0200

    gouraud

--- shader/shader.frag.glsl
+++ shader/shader.frag.glsl
@@ -1,36 +1,11 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec3 ourColor;
-in vec3 Normal;
-in vec3 FragPos;
-in vec2 TexCoord;
-in vec3 LightPos;
+in vec3 LightingColor;
 
-uniform sampler2D ourTexture;
 uniform vec3 objectColor;
-uniform vec3 lightColor;
-uniform vec3 viewPos;
 
 void main()
 {
-   // FragColor = texture(ourTexture, TexCoord);// * vec4(ourColor, 1.0);
-   float ambientStrength = 0.2;
-   float diffuseStrength = 1;
-   float specularStrength = 0.3;
-
-   vec3 ambient = ambientStrength * lightColor;
-
-   vec3 norm = normalize(Normal);
-   vec3 lightDir = normalize(LightPos - FragPos);
-   float diff = max(dot(norm, lightDir), 0.0);
-   vec3 diffuse = diffuseStrength * diff * lightColor;
-
-   vec3 viewDir = normalize(viewPos - FragPos);
-   vec3 reflectDir = reflect(-lightDir, norm);  
-   float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-   vec3 specular = specularStrength * spec * lightColor;
-
-   vec3 result = (ambient + diffuse + specular) * objectColor;
-   FragColor = vec4(result, 1.0);
+    FragColor = vec4(LightingColor * objectColor, 1.0);
 }

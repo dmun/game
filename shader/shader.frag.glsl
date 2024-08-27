dfd9ccf1b2825aa4b1477a211eac298b9800e41d
dmun <davidru01@live.nl>
2024-08-27 18:28:27 +0200

    texture

--- shader/shader.frag.glsl
+++ shader/shader.frag.glsl
@@ -5,10 +5,10 @@ in vec3 LightingColor;
 in vec3 Normal;
 in vec3 FragPos;
 in vec3 LightPos;
+in vec2 TexCoords;
 
 struct Material {
-    vec3 ambient;
-    vec3 diffuse;
+    sampler2D diffuse;
     vec3 specular;
     float shininess;
 };
@@ -27,12 +27,12 @@ uniform Light light;
 
 void main()
 {
-    vec3 ambient = light.ambient * material.ambient;
+    vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
 
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(LightPos - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = light.diffuse * material.diffuse * diff;
+    vec3 diffuse = light.diffuse * vec3(texture(material.diffuse, TexCoords)) * diff;
 
     vec3 viewDir = normalize(-FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);  
--- shader/shader.vert.glsl
+++ shader/shader.vert.glsl
@@ -1,11 +1,13 @@
 #version 330 core
 layout(location = 0) in vec3 aPos;
 layout(location = 1) in vec3 aNormal;
+layout(location = 2) in vec2 aTexCoords;
 
 out vec3 LightingColor;
 out vec3 FragPos;
 out vec3 Normal;
 out vec3 LightPos;
+out vec2 TexCoords;
 
 uniform vec3 lightPos;
 uniform mat4 model;
@@ -19,4 +21,5 @@ void main()
     FragPos = vec3(view * model * vec4(aPos, 1.0));
     Normal = mat3(transpose(inverse(view * model))) * aNormal;
     LightPos = vec3(view * vec4(lightPos, 1.0));
+    TexCoords = aTexCoords;
 }
